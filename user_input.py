0aa51631df74fdc4a7467b2585f6edd191e72eb0
Saidi Bakari Namtanga <157792603+Namtanga@users.noreply.github.com>
2024-03-01 05:42:45 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# Ask the user for their name and store it in a variable called "name"
+name = input("What is your name? ")
+# Print a greeting using the provided name
+print("Hello,", name + "!")                                                                                                  # Ask the user for their age and store it in a variable called "age"
+age = input("How old are you? ")
+
+# Print a confirmation message
+print("Your age is:", age)                                                                                                    
